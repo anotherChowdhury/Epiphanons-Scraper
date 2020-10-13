4231359617e7c503b7c3fb40f2dd94e544b23e7b
anotherChowdhury <umarfaruk.chowdhury@gmail.com>
2020-10-13 16:52:33 +0600

    ISO-TIME

--- scraper.py
+++ scraper.py
@@ -2,15 +2,19 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 import os
 import json
+from datetime import datetime
 
 
 def get_data(URL):
+    if(not URL.startswith('https://www.facebook.com')):
+        return ({"message": "Please send a link of a public facebook post"})
     chrome_options = Options()
     chrome_options.add_argument('--headless')
     chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--no-sandbox")
-    browser = webdriver.Chrome(executable_path = os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
+    browser = webdriver.Chrome(executable_path=os.environ.get(
+        "CHROMEDRIVER_PATH"), chrome_options=chrome_options)
     browser.get(URL)
     try:
         picture_link = browser.find_element_by_css_selector(
@@ -18,6 +22,7 @@ def get_data(URL):
         # print(picture_link)
     except Exception as e:
         print(e)
+        return ({"message": "Error Occured while scraping picture link."})
     try:
         name = browser.find_element_by_css_selector(
             '.fwb.fcg').text.strip()
@@ -26,25 +31,30 @@ def get_data(URL):
         # print(name)
         # print(profile_link)
     except Exception as e:
-        print(e)
-        if('&id=' in URL):
-            profile_link = "https://www.facebook.com/profile.php/" + \
-                URL.split("=")[-1].strip()
-        else:
-            profile_link = URL.split('post')[0].strip()
+        try:
+            if('&id=' in URL):
+                profile_link = "https://www.facebook.com/profile.php/" + \
+                    URL.split("=")[-1].strip()
+            else:
+                profile_link = URL.split('post')[0].strip()
+        except:
+            return ({"message": "Error Occured while scraping profile link"})
     try:
         post_text = browser.find_element_by_css_selector(
             'div[data-testid="post_message"]').text.strip()
         # print(post_text)
     except Exception as e:
         print(e)
+        return ({"message": "Error Occured whle scaping post text."})
     try:
         time_of_post = browser.find_element_by_css_selector(
-            '.timestampContent').text.strip()
+            'abbr').get_attribute('title').strip()
+        time_of_post = datetime.strptime(time_of_post, "%A, %d %B %Y at %H:%M")
         print(time_of_post)
+        time_of_post = time_of_post.isoformat()
     except Exception as e:
         print(e)
-
+        return ({"message": "Error Occured while scraping time."})
     browser.quit()
 
     return ({
@@ -55,10 +65,3 @@ def get_data(URL):
         "text": post_text,
         "image": picture_link
     })
-
-    # for object in respone_object:
-    #     print(json.loads(object))
-    # try:
-    #     browser.find_element_by_css_selector('button[name="login"]').click()
-    # except:
-    #     browser.find_element_by_css_selector('input[value="Log In"]').click()
